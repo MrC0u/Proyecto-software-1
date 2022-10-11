242d398f42ae37808e99c0cef3adb74e1ba5bbb9
MrCou_ <88692884+MrC0u@users.noreply.github.com>
2022-10-11 04:55:00 -0300

    Update Login.js

--- react/src/pages/Login/Login.js
+++ react/src/pages/Login/Login.js
@@ -1,127 +1,125 @@
-import React, {useState} from "react";
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-import { Title } from "./Components/Title";
-import { Label } from "./Components/Label";
-import { Input } from "./Components/Input";
-
+import {
+    Button,
+    TextField,
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    Box,
+} from "@mui/material";
 
 export const Login = () => {
-    const [ user, setUser ] = useState('')
-    
-    const [ password, setPassword ] = useState('')
-    const [ passwordError, setPasswordError] = useState(false)
-    
-    const [ isLogin, setIsLogin] = useState(false)
-    const [ hasError, setHasError] = useState(false)
 
-    function handleChange(name, value){
-        if(name === 'usuario'){
-            setUser(value)
-        }else{
-            if(value.length<1){
-                setPasswordError(true)
-            }else{
-                setPasswordError(false)
-                setPassword(value)
-            }
-        }
+    const [datos, setDatos] = useState({
+        usuario: '',
+        clave: '',
+    })
+
+    const handleChange = e => {
+        setDatos({ ...datos, [e.target.name]: e.target.value })
     }
-
 
     const Navigate = useNavigate();
 
-    const ifMatch = async (param) => {
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
         try {
 
-            if(param.user.length >0 && param.password.length >0){
-                const response = await fetch(`http://localhost:4000/login/${param.user}`,{
+            const response = await fetch(`http://localhost:4000/login/${datos.usuario}`, {
+                method: 'GET',
+            });
+            const data = await response.json();
+
+            if (datos.clave === data[0].clave) {
+
+                const response = await fetch(`http://localhost:4000/userLevel/${datos.usuario}`, {
                     method: 'GET',
                 });
                 const data = await response.json();
-                if(param.password === data[0].clave){
+
+                if (data[0].codigo === 1) {
+                    Navigate("/admin");
+                } else {
 
 
-                    const response = await fetch(`http://localhost:4000/userLevel/${param.user}`,{
-                    method: 'GET',
+                    const response = await fetch(`http://localhost:4000/getId/${datos.usuario}`, {
+                        method: 'GET',
                     });
                     const data = await response.json();
-                    setIsLogin(true);
-                    if(data[0].codigo === 1){
-                        Navigate("/admin");
-                    }else{
 
-
-                        const response = await fetch(`http://localhost:4000/getId/${param.user}`,{
-                        method: 'GET',
-                        });
-                        const data = await response.json();
-
-                        Navigate(`/vendedor/${data[0].id}`);
-                    }
-                    
-                    
-                }else{
-                    setIsLogin(false);
-                    setHasError(true);
+                    Navigate(`/vendedor/${data[0].id}`);
                 }
-    
-            }else{
-                console.log(param)
-                setIsLogin(false);
-                setHasError(true)
+
+            } else {
+                console.log(datos)
             }
         } catch (error) {
             console.log(error);
         }
-       
-    }
-
-
-    function handleSubmit(){
-        let account = { user, password }
-        if(account){
-            ifMatch(account);
-        }
-    }
+    };
 
     return (
-        <div className= "login-container" >
-            <Title text ="¡Bienvenido!"/>
-            { hasError &&
-                <label className="label-alert">
-                    Su contraseña o usuario son incorrectos, o no existe en nuestra plataforma
-                </label>
-            }
-            <Label text= "Usuario"/>
-            <Input attribute={{
-                id:'usuario',
-                name:'usuario',
-                type:'text',
-                placeholder:'Ingrese usuario'
-            }}
-            handleChange={handleChange}
-            />
-            <Label text= "Contraseña"/>
-            <Input attribute={{
-                id:'contraseña',
-                name:'contraseña',
-                type:'password',
-                placeholder:'Ingrese contraseña'
-            }}
-            handleChange={handleChange}
-            param={passwordError}
-            />
+        <div className="login-container" >
 
-            {passwordError &&
-                <label>
-                    Contraseña invalida o incompleta
-                </label> 
-            }
-        
-            <button onClick={handleSubmit}>
-                Ingresar
-            </button>
+
+            <Grid container direction='column' alignItems='center' justifyContent='center' style={{ minHeight: '75vh' }}>
+                <Grid item xs={3}>
+                    <Card sx={{ mt: 5 }} style={{
+                        backgroundColor: "#1e272e",
+                        padding: "2rem",
+                    }}>
+                        <Typography variant='h5' textAlign='center' color='white'>
+                            Inicio de Sesión
+                        </Typography>
+                        <Box textAlign='center'>
+                            <CardContent >
+                                <form onSubmit={handleSubmit}>
+
+                                    <TextField
+                                        variant='outlined'
+                                        label='Usuario '
+                                        sx={{
+                                            display: 'block',
+                                            margin: '1rem 0'
+                                        }}
+
+                                        name='usuario'
+                                        onChange={handleChange}
+                                        inputProps={{ style: { color: "white" } }}
+                                        InputLabelProps={{ style: { color: "white" } }}
+                                    />
+
+                                    <TextField
+                                        variant='outlined'
+                                        label='Clave '
+                                        sx={{
+                                            display: 'block',
+                                            margin: '1rem 0'
+                                        }}
+
+                                        name='clave'
+                                        onChange={handleChange}
+                                        inputProps={{ style: { color: "white" } }}
+                                        InputLabelProps={{ style: { color: "white" } }}
+                                    />
+
+                                    <Button variant='contained' color='primary' type='submit'>
+                                        Ingresar
+                                    </Button>
+
+                                </form>
+                            </CardContent>
+                        </Box>
+                    </Card>
+
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
